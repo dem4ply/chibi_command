af9d03e3cde25fc69a73fb69b8c3b88342c450f1
dem4ply <dem4ply@gmail.com>
2020-02-21 21:54:06 -0600

    Bump version: 0.0.8 → 0.0.9

--- setup.py
+++ setup.py
@@ -33,6 +33,6 @@ setup(
     name='chibi_command',
     packages=find_packages(include=['chibi_command', 'chibi_command.*']),
     url='https://github.com/dem4ply/chibi_command',
-    version='0.0.8',
+    version='0.0.9',
     zip_safe=False,
 )
