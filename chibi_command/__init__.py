66989c8e421fc4d5fa44cb0890e50007752e1be5
dem4ply <dem4ply@gmail.com>
2020-03-19 17:56:34 -0600

    Bump version: 0.1.6 → 0.2.0

--- chibi_command/__init__.py
+++ chibi_command/__init__.py
@@ -6,7 +6,7 @@ import logging
 
 __author__ = """dem4ply"""
 __email__ = 'dem4ply@gmail.com'
-__version__ = '0.1.6'
+__version__ = '0.2.0'
 
 logger = logging.getLogger( 'chibi.command' )
 
--- setup.py
+++ setup.py
@@ -33,6 +33,6 @@ setup(
     name='chibi_command',
     packages=find_packages(include=['chibi_command', 'chibi_command.*']),
     url='https://github.com/dem4ply/chibi_command',
-    version='0.1.6',
+    version='0.2.0',
     zip_safe=False,
 )
