9795caf27417611bc8b985c3c255632ebb2e3d3a
dem4ply <dem4ply@gmail.com>
2026-03-25 11:03:45 -0600

    se agrego funcion para buscar packates de pacman

--- chibi_command/archilinux/pacman.py
+++ chibi_command/archilinux/pacman.py
@@ -1,4 +1,30 @@
-from chibi_command import Command
+from chibi_atlas import Chibi_atlas
+from chibi_command import Command, Command_result
+from chibi_hybrid import Chibi_hybrid
+
+
+def join_parted_lines( lines ):
+    prev = next( lines )
+    for line in lines:
+        if line.startswith( ' ' ):
+            prev = f"{prev}\n{line}"
+        else:
+            yield prev
+            prev = line
+    yield prev
+
+
+class Query_result( Command_result ):
+    def parse_result( self ):
+        if not bool( self ):
+            return
+        lines = self.result.split( '\n' )
+        lines = filter( bool, lines )
+        lines = join_parted_lines( lines )
+        tuples = ( line.split( ':', 1 ) for line in lines )
+        tuples = ( ( k.strip().lower(), v.strip() ) for k, v in tuples )
+        result = Chibi_atlas( dict( tuples ) )
+        self.result = result
 
 
 class Pacman( Command ):
@@ -16,3 +42,20 @@ class Pacman( Command ):
     @classmethod
     def install( cls, *packages ):
         return cls( '-S', *packages )
+
+    @classmethod
+    def query( cls ):
+        return cls( '-Q' )
+
+    @Chibi_hybrid
+    def info( cls, package ):
+        return cls.query().info( package )
+
+    @info.instancemethod
+    def info( self, package ):
+        self.add_args( '-i', package )
+        self.env.LC_ALL = 'C'
+        self.result_class = Query_result
+        self.captive = True
+        self.raise_on_fail = False
+        return self
--- tests/archlinux/pacman.py
+++ tests/archlinux/pacman.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from chibi_command.archilinux import Pacman
+from chibi_command.archilinux.pacman import join_parted_lines
 
 
 class Test_pacman( TestCase ):
@@ -17,3 +18,42 @@ class Test_pacman( TestCase ):
         pacman = Pacman.install( 'python-chibi', 'python-chibi-command' )
         expected = 'pacman -S python-chibi python-chibi-command'
         self.assertEqual( pacman.preview(), expected )
+
+    def test_preview_query_info_package( self ):
+        pacman = Pacman.query().info( 'sudo' )
+        expected = 'pacman -Q -i sudo'
+        self.assertEqual( pacman.preview(), expected )
+
+    def test_query_info_result_should_work( self ):
+        pacman = Pacman.query().info( 'sudo' )
+        result = pacman.run()
+        self.assertTrue( result )
+
+    def test_query_info_result_should_have_name( self ):
+        pacman = Pacman.query().info( 'sudo' )
+        result = pacman.run()
+        self.assertIn( 'name', result.result )
+        self.assertEqual( result.result.name, 'sudo' )
+
+    def test_query_info_with_no_package_should_return_false( self ):
+        pacman = Pacman.query().info( 'adsffdas' )
+        result = pacman.run()
+        self.assertFalse( result )
+
+
+class Test_join_parted_lines( TestCase ):
+    def test_should_work( self ):
+        example = [
+            'Depends On      : pacman>6.1  git',
+            'Optional Deps   : sudo: privilege elevation [installed]',
+            '                  doas: privilege elevation',
+        ]
+        result = list( join_parted_lines( iter( example ) ) )
+        self.assertEqual(
+            result,
+            [
+                'Depends On      : pacman>6.1  git',
+                'Optional Deps   : sudo: privilege elevation [installed]\n'
+                '                  doas: privilege elevation',
+            ]
+        )
